87ad9275dbbc35d66f4ff278f49268dec5d972f6
kver <kevin.verhaegh@ukaea.uk>
2022-04-14 19:03:24 +0100

    Bugfix for Read_TempF to support single chord data. Bugfix of BaSPMI for Te < 0.5 eV. Update to support low Te ADAS data (experimental)

---
+++ DSS_Auxiliary_Routines/matlab_home.m
@@ -0,0 +1,2 @@
+function dum=matlab_home()
+dum = '/home/kver/PycharmProjects/solps_tools/solps_tools/matlab_tcv_export';
---
+++ DSS_Auxiliary_Routines/python_home.m
@@ -0,0 +1,3 @@
+function dum=python_home()
+dum = '/common/projects/diagnostics/MAST/SPEXBDMS/venv/bin/activate';
+dum = '/home/kver/PycharmProjects/dms_nf_letter/venv/bin/activate';
